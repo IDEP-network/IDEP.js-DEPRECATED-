624f21199664dd723cb7d9db0925fb898fbf081a
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-10-29 02:00:42 +0200

    updated tests

--- test/crypto.test.ts
+++ test/crypto.test.ts
@@ -1,7 +1,5 @@
 import * as crypto from '../src/utils/crypto';
 
-const CryptoJS = require('crypto-js') as typeof import('crypto-js');
-
 const readyMnemo =
   'cost unfold manage group pen cradle agree library rose lizard proof supply canvas assault orchard patch zebra absurd useful upset south glide miracle ostrich';
 
@@ -20,7 +18,13 @@ const readyAddress = 'idep19hv30xxwzk3q04h9jh4ct7z09tutuhst5jewev';
 const pbkdfSalt = '733f6f81592b8f53c3134bec4556a55d';
 
 const readyPbkdfHex =
-  'e20d02b49224af62afce8b544d1e785b390cc34cb1aec85997487f85ccdbe9f7ca674049c3e3f747409166c1a5e66d6550762a3bd88a30137024cce3b4f6912a72b71d72722975e5b7fb9b22726a548dcb34bc9172b53751c06763757a98b13582defbda57b3f8926ba9db3a1a0bb2fc806c25c0cd57d0e7b12d40d40af09c38';
+  'a80835778e547212e690b24e88471cc6d58c158eaba7301352ea73669d85a5f7';
+
+const readyEncrypted = {
+  cipherText: 'a7a2ddb3c965274a7772c5b5',
+  iv: '9ae2ba46aa3c30afc74efe9900000000',
+  salt: 'b0477184762087f04b7e204cc35429f0',
+};
 
 describe('Crypto module', () => {
   it('getRandomBytes returns correct number of bytes', async () => {
@@ -60,22 +64,19 @@ describe('Crypto module', () => {
   });
   it('PBKDF2 works fine', async () => {
     const pwd = 'randompwd';
-    const salt = CryptoJS.enc.Hex.parse(pbkdfSalt);
-    const key = crypto.pbkdf2(pwd, salt);
-    expect(key.toString(CryptoJS.enc.Hex)).toBe(readyPbkdfHex);
+    const key = await crypto.pbkdf2(pwd, Buffer.from(pbkdfSalt, 'hex'));
+    expect(key.toString('hex')).toBe(readyPbkdfHex);
   });
   it('Aes  encrypt works', async () => {
     const pwd = 'randompwd';
     const msg = 'test message';
     const encrypted = await crypto.aesEncrypt(msg, pwd);
-    console.log(encrypted);
+    const decrypted = await crypto.aesDecrypt(encrypted, pwd);
+    expect(decrypted).toEqual('test message');
   });
   it('Decrypt works  too', async () => {
     const pwd = 'randompwd';
-    const msg = 'test message';
-    const encrypted = await crypto.aesEncrypt(msg, pwd);
-    const decrypted = await crypto.aesDecrypt(encrypted, pwd);
-    console.log(decrypted);
+    const decrypted = await crypto.aesDecrypt(readyEncrypted, pwd);
     expect(decrypted).toEqual('test message');
   });
 });
