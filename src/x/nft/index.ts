78ab8c5941fd13ff9cb48a69a5da2343a9e8777e
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-06 08:08:55 +0100

    nft queries

---
+++ src/x/nft/index.ts
@@ -0,0 +1,75 @@
+import {Client} from '../../client';
+import {
+ queryCollectionRequest,
+ queryDenomRequest,
+ queryMultipleDenomsRequest,
+ queryNFTRequest,
+ queryOwnerRequest,
+ querySupplyRequest
+} from '../../types/ion.uptake';
+
+export class Nft {
+  client: Client;
+  constructor(client: Client) {
+    this.client = client;
+  }
+
+  async checkSupply(denomId?: string, owner?: string) {
+    const [query, protoResponse] = querySupplyRequest(denomId, owner);
+    const params = await this.client.rpc.abciQuery(
+      '/idep.ion.uptake.Query/Supply', // remove emagic strings
+      query,
+      protoResponse
+    );
+    return params;
+  }
+
+  async checkOwner(owner: string, denomId?: string) {
+    const [query, protoResponse] = queryOwnerRequest(owner, denomId);
+    const params = await this.client.rpc.abciQuery(
+      '/idep.ion.uptake.Query/Owner', // remove emagic strings
+      query,
+      protoResponse
+    );
+    return params;
+  }
+  async checkCollection(denomId: string) {
+    const [query, protoResponse] = queryCollectionRequest(denomId);
+    const params = await this.client.rpc.abciQuery(
+      '/idep.ion.uptake.Query/Collection', // remove emagic strings
+      query,
+      protoResponse
+    );
+    return params;
+  }
+
+  async checkDenom(denomId: string) {
+    const [query, protoResponse] = queryDenomRequest(denomId);
+    const params = await this.client.rpc.abciQuery(
+      '/idep.ion.uptake.Query/Denom', // remove emagic strings
+      query,
+      protoResponse
+    );
+    return params;
+  }
+
+  async checkMultipleDenoms() {
+    const [query, protoResponse] = queryMultipleDenomsRequest();
+    const params = await this.client.rpc.abciQuery(
+      '/idep.ion.uptake.Query/Denoms', // remove emagic strings
+      query,
+      protoResponse
+    );
+    return params;
+  }
+
+  async checkNft(denomId: string, tokenId: string) {
+    const [query, protoResponse] = queryNFTRequest(denomId, tokenId);
+    const params = await this.client.rpc.abciQuery(
+      '/idep.ion.uptake.Query/NFT', // remove emagic strings
+      query,
+      protoResponse
+    );
+    return params;
+  }
+}
