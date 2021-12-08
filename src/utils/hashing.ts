f8700a747a8ca45863167874f4b4dc5eae1f26a6
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-08 20:54:48 +0100

    Create hashing.ts

---
+++ src/utils/hashing.ts
@@ -0,0 +1,32 @@
+import cryptoAsync from '@ronomon/crypto-async';
+
+const isNode =
+  typeof process !== 'undefined' &&
+  process.versions != null &&
+  process.versions.node != null;
+if (isNode) {
+  var Buffer = require('buffer').Buffer;
+} else {
+  var Buffer = require('buffer/').Buffer;
+}
+export const sha256 = (bytes: Uint8Array | Buffer): Promise<Buffer> => {
+  const buffer = bytes instanceof Buffer ? bytes : Buffer.from(bytes);
+
+  return new Promise((resolve, reject) => {
+    cryptoAsync.hash('sha256', buffer, (err: Error, hash: Buffer) => {
+      if (err) reject(err);
+      else resolve(hash);
+    });
+  });
+};
+
+export const ripemd160 = (bytes: Uint8Array | Buffer): Promise<Buffer> => {
+  const buffer = bytes instanceof Buffer ? bytes : Buffer.from(bytes);
+
+  return new Promise((resolve, reject) => {
+    cryptoAsync.hash('ripemd160', buffer, (err: Error, hash: Buffer) => {
+      if (err) reject(err);
+      else resolve(hash);
+    });
+  });
+};
