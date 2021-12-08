2b2174e573552723defe2d586b2bb99af7f08d6b
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-08 20:55:46 +0100

    Update index.ts

--- src/x/bank/index.ts
+++ src/x/bank/index.ts
@@ -1,10 +1,35 @@
 import {ClientInterfce} from '../../client';
-import {queryAllBalancesRequest, queryBalanceRequest, queryParamsRequest, querySupplyOfRequest} from '../../types/bank';
+import {
+  MsgSend,
+  queryAllBalancesRequest,
+  queryBalanceRequest,
+  queryParamsRequest,
+  querySupplyOfRequest
+} from '../../types/bank';
+import {Coin, StdFee} from '../../types/types';
+import {Bech32Address, HexEncoded} from '../types/aliases';
 
 export class Bank {
   client: ClientInterfce;
   constructor(client: ClientInterfce) {
     this.client = client;
+  }
+  async msgSendTx(
+    recipient: Bech32Address,
+    amount: Coin[],
+    baseTx: {
+      from: Bech32Address;
+      pub_key: HexEncoded;
+      gas?: string | undefined;
+      fee?: StdFee | undefined;
+      memo?: string | 'No memes for you';
+    } // TODO note
+  ) {
+    // TODO validate addrresses
+    const msgs: any[] = [
+      new MsgSend({ fromAddress: recipient, toAddress: baseTx.from, amount }),
+    ];
+    return this.client.tx.buildSignSend(msgs, baseTx);
   }
   async checkBalance(address: string, denom: string) {
     const [query, protoResponse] = queryBalanceRequest(address, denom);
