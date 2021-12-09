6a7f4156aef1b518bd930966394aa28fbda32ece
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-09 17:40:15 +0100

    added bank's tx mgs

--- src/types/bank.ts
+++ src/types/bank.ts
@@ -1,4 +1,8 @@
+import {GeneralTxTools} from '../x/tx/genTx';
+import {Bech32Address, ProtoBuffObject, ProtoBuffType} from '../x/types/aliases';
+import {MsgType} from './msg_types';
 import * as pbs from './proto';
+import {Coin} from './types';
 
 export const queryBalanceRequest = (
   address: string,
@@ -26,3 +30,74 @@ export const queryParamsRequest = () => {
   const query = new pbs.bank_query_pb.QueryParamsRequest();
   return [query, pbs.bank_query_pb.QueryParamsResponse];
 };
+
+export class MsgSend {
+  value: MsgSendValue;
+  type: MsgType;
+  private static _protoBuffType: ProtoBuffType = pbs.bank_tx_pb.MsgSend;
+  constructor(message: MsgSendValue) {
+    this.type = MsgType.MsgSend;
+    this.value = message;
+  }
+  static getProtoBuffType(): ProtoBuffType {
+    return this._protoBuffType;
+  }
+  protoBuffObject(): ProtoBuffObject {
+    let protoMsg = new pbs.bank_tx_pb.MsgSend();
+    protoMsg.setFromAddress(this.value.fromAddress);
+    protoMsg.setToAddress(this.value.toAddress);
+    this.value.amount.forEach(el => {
+      protoMsg.addAmount(GeneralTxTools.createCoin(el));
+    });
+    return protoMsg;
+  }
+}
+
+export class MsgMultiSend {
+  value: MsgMultiSendValue;
+  type: MsgType;
+  private static _protoBuffType: ProtoBuffType = pbs.bank_tx_pb.MsgMultiSend;
+  constructor(message: MsgMultiSendValue) {
+    this.type = MsgType.MsgMultiSend;
+    this.value = message;
+  }
+  static getProtoBuffType(): ProtoBuffType {
+    return this._protoBuffType;
+  }
+  protoBuffObject(): ProtoBuffObject {
+    let protoMsg = new pbs.bank_tx_pb.MsgMultiSend();
+    this.value.inputs.forEach(el => {
+      let input = new pbs.bank_tx_pb.Input();
+      input.setAddress(el.address);
+      el.coins.forEach(coin => {
+        input.addCoins(GeneralTxTools.createCoin(coin));
+      });
+      protoMsg.addOutputs(input);
+    });
+    this.value.outputs.forEach(el => {
+      let output = new pbs.bank_tx_pb.Output();
+      output.setAddress(el.address);
+      el.coins.forEach(coin => {
+        output.addCoins(GeneralTxTools.createCoin(coin));
+      });
+      protoMsg.addOutputs(output);
+    });
+    return protoMsg;
+  }
+}
+
+interface InputOrOutput {
+  address: Bech32Address;
+  coins: Coin[];
+}
+
+interface MsgSendValue {
+  fromAddress: Bech32Address;
+  toAddress: Bech32Address;
+  amount: Coin[];
+}
+
+interface MsgMultiSendValue {
+  inputs: InputOrOutput[];
+  outputs: InputOrOutput[];
+}
