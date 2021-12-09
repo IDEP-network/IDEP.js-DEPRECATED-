822bf3575cc46f2e0476c1a05175d6b73b172a67
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-09 19:32:52 +0100

    Update index.ts

--- src/x/bank/index.ts
+++ src/x/bank/index.ts
@@ -1,6 +1,9 @@
 import {ClientInterfce} from '../../client';
 import {
+  InputOrOutput,
+  MsgMultiSend,
   MsgSend,
+  MsgSendValue,
   queryAllBalancesRequest,
   queryBalanceRequest,
   queryParamsRequest,
@@ -9,14 +12,18 @@ import {
 import {Coin, StdFee} from '../../types/types';
 import {Bech32Address, HexEncoded} from '../types/aliases';
 
+interface SendTransfer {
+  recipient: Bech32Address;
+  amount: Coin[];
+}
+
 export class Bank {
   client: ClientInterfce;
   constructor(client: ClientInterfce) {
     this.client = client;
   }
   async msgSendTx(
-    recipient: Bech32Address,
-    amount: Coin[],
+    transfer: SendTransfer,
     baseTx: {
       from: Bech32Address;
       pub_key: HexEncoded;
@@ -26,9 +33,28 @@ export class Bank {
     } // TODO note
   ) {
     // TODO validate addrresses
-    const msgs: any[] = [
-      new MsgSend({ fromAddress: recipient, toAddress: baseTx.from, amount }),
-    ];
+    const value = ({
+      ...transfer,
+      fromAddress: baseTx.from,
+    } as unknown) as MsgSendValue;
+
+    const msgs: any[] = [new MsgSend(value)];
+    return this.client.tx.buildSignSend(msgs, baseTx);
+  }
+
+  async msgMultiSendTx(
+    inputs: InputOrOutput[],
+    outputs: InputOrOutput[],
+    baseTx: {
+      from: Bech32Address;
+      pub_key: HexEncoded;
+      gas?: string | undefined;
+      fee?: StdFee | undefined;
+      memo?: string | 'No memes for you';
+    } // TODO note
+  ) {
+    // TODO validate addrresses
+    const msgs: any[] = [new MsgMultiSend({ inputs, outputs })];
     return this.client.tx.buildSignSend(msgs, baseTx);
   }
   async checkBalance(address: string, denom: string) {
