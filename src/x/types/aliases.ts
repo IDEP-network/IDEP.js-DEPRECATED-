2336f0edba593be628a4bde63544234bdbcf9757
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-09 14:51:37 +0100

    Create aliases.ts

---
+++ src/x/types/aliases.ts
@@ -0,0 +1,9 @@
+export type ProtoBuffObject = any;
+
+export type ProtoBuffType = any;
+
+export type HexEncoded = string;
+
+export type Base64Encoded = string;
+
+export type Base58Encoded = string;
