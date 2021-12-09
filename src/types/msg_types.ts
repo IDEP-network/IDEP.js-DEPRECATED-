0f87ef626cd52e701a99c90f62180dc8cd9b5d5b
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-09 17:39:22 +0100

    Create msg_types.ts

---
+++ src/types/msg_types.ts
@@ -0,0 +1,10 @@
+export enum MsgType {
+  MsgSend = 'cosmos.bank.v1beta1.MsgSend',
+  MsgMultiSend = 'cosmos.bank.v1beta1.MsgMultiSend',
+  //nft
+  MsgIssueDenom = 'idep.ion.uptake.nft.MsgIssueDenom',
+  MsgTransferNFT = 'idep.ion.uptake.nft.MsgTransferNFT',
+  MsgEditNFT = 'idep.ion.uptake.nft.MsgEditNFT',
+  MsgMintNFT = 'idep.ion.uptake.nft.MsgMintNFT',
+  MsgBurnNFT = 'idep.ion.uptake.nft.MsgBurnNFT',
+}
