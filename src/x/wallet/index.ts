8fb099c0200d975c7d9f1a13cf8e54fc8faf61a1
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2021-12-29 05:27:55 +0100

    optimized new wallet creation, correct public key  encoding/formatting for human interaction, streamlined restoring wallet from mnemonics or private key

--- src/x/wallet/index.ts
+++ src/x/wallet/index.ts
@@ -1,7 +1,9 @@
+import {bech32} from 'bech32';
+
 import {EncryptedPrivateKey, EncryptedWallet} from '../../types/types';
 import * as walletCreationTool from '../../utils/account';
 import {CryptoStrategy, EncryptionTool} from '../../utils/crypto_strategy/crypto_strategy';
-import {HexEncoded} from '../types/aliases';
+import {Bech32Address, HexEncoded} from '../types/aliases';
 import {PersistentStorage, Store, WalletJson} from './store';
 
 export class WalletUtilities {
@@ -40,9 +42,9 @@ export class Wallet {
   // TODO add getters for wallet's data to check for cases when it isn't available
   publicKey: HexEncoded;
   address: string;
+  name?: string;
   privateKey: EncryptedPrivateKey;
   encryptionTool: CryptoStrategy;
-  storedWalletsNames: string[];
   store: PersistentStorage;
   constructor(encryptionTool: CryptoStrategy, store: PersistentStorage) {
     this.encryptionTool = encryptionTool;
@@ -50,52 +52,110 @@ export class Wallet {
   }
   async listSavedWallets() {
     const storedWallets = await this.store.getSavedWallets();
-    this.storedWalletsNames = storedWallets;
     return storedWallets;
   }
   async retrieveSavedWallet(name: string): Promise<WalletJson> {
     const wallet = await this.store.pickWallet(name);
-    const { pub_key, address, crypto } = wallet;
-    this.publicKey = pub_key;
+    console.log(wallet);
+    const { address, crypto } = wallet;
+    // TODO obvious lol
+    const publicKey = wallet.pub_key || wallet.publicKey;
+    this.publicKey = publicKey;
     this.address = address;
     this.privateKey = crypto;
     return wallet;
   }
 
-  async storeWalletInMemory(password, privateKey, publicKey, address) {
-    this.privateKey = await this.encryptionTool.encrypt(
+  async storeWalletInMemory(
+    privateKey: EncryptedPrivateKey,
+    publicKey: HexEncoded,
+    address: Bech32Address,
+    name?: string
+  ): Promise<void> {
+    this.privateKey = privateKey;
+    this.publicKey = publicKey;
+    this.address = address;
+    this.name = name || address;
+  }
+  async persistWallet(
+    name?: string,
+    encryptedWallet?: EncryptedWallet
+  ): Promise<void> {
+    if (!encryptedWallet) {
+      const randomBytes = await this.encryptionTool.getRandomBytes(12);
+      encryptedWallet = {
+        name: name || this.address,
+        id: Buffer.from(randomBytes).toString('hex'),
+        publicKey: this.publicKey,
+        address: this.address,
+        crypto: this.privateKey,
+      };
+    }
+    await this.store.saveWallet(encryptedWallet.name, encryptedWallet);
+  }
+  async createNew(password: string, name?: string) {
+    const walletPromise = WalletUtilities.generateWallet();
+    const ramdomBytesPromise = this.encryptionTool.getRandomBytes(12);
+    const promiseResults = await Promise.allSettled([
+      walletPromise,
+      ramdomBytesPromise,
+    ]);
+    const [wallet, ramdomBytes] = promiseResults.map(
+      el => (el as PromiseFulfilledResult<any>).value
+    );
+    const { privateKey, address, mnemonic } = wallet;
+    const publicKeyRaw = wallet.publicKey || wallet.pub_key;
+    const encryptedPrivate = await this.encryptionTool.encrypt(
       privateKey.toString('hex'),
       password
     );
-    this.publicKey = publicKey.toString('hex');
-    this.address = address;
-  }
-  async persistWallet(name: string) {
-    const ramdomBytes = await this.encryptionTool.getRandomBytes(12);
     const encryptedWallet: EncryptedWallet = {
-      name,
+      name: name || this.address,
       id: Buffer.from(ramdomBytes).toString('hex'),
-      pub_key: this.publicKey,
-      address: this.address,
-      crypto: this.privateKey,
+      publicKey: publicKeyRaw.toString('hex'),
+      address: address,
+      crypto: encryptedPrivate,
     };
-    await this.store.saveWallet(name, encryptedWallet);
+    const storePromise = this.storeWalletInMemory(
+      encryptedWallet.crypto,
+      encryptedWallet.publicKey,
+      encryptedWallet.address,
+      encryptedWallet.name
+    );
+    const persistPromise = this.persistWallet(
+      encryptedWallet.name,
+      encryptedWallet
+    );
+    await Promise.all([storePromise, persistPromise]);
+    const publicKey = this.formatPublicKeyForUserInteraction(publicKeyRaw);
+    return { mnemonic, publicKey, address };
   }
-  async createNew(password: string) {
-    const wallet = await WalletUtilities.generateWallet();
-    const { privateKey, publicKey, address, mnemonic } = wallet;
-    await this.storeWalletInMemory(password, privateKey, publicKey, address);
-    return mnemonic;
-  }
-  async restoreFromSeed(password: string, mnemonic: string) {
+  async restoreFromSeed(password: string, mnemonic: string): Promise<any> {
     const wallet = await WalletUtilities.recoverFromMnemonics(mnemonic);
-    const { privateKey, publicKey, address } = wallet;
-    await this.storeWalletInMemory(password, privateKey, publicKey, address);
+    return this.handleRestoredWallet(wallet, password);
   }
-  async restoreFromPrivateKey(privKey: HexEncoded, password: string) {
+  async restoreFromPrivateKey(
+    privKey: HexEncoded,
+    password: string
+  ): Promise<any> {
     const wallet = await WalletUtilities.recoverFromPrivateKey(privKey);
-    const { privateKey, publicKey, address } = wallet;
-    await this.storeWalletInMemory(password, privateKey, publicKey, address);
+    return this.handleRestoredWallet(wallet, password);
+  }
+  async handleRestoredWallet(wallet, password) {
+    const { privateKey, publicKey: publicKeyRaw, address } = wallet;
+    const encryptedPrivate = await this.encryptionTool.encrypt(
+      privateKey.toString('hex'),
+      password
+    );
+    const publicKey = this.formatPublicKeyForUserInteraction(
+      publicKeyRaw.toString('hex')
+    );
+    await this.storeWalletInMemory(
+      encryptedPrivate,
+      publicKeyRaw.toString('hex'),
+      address
+    );
+    return { publicKey, address };
   }
   async getPrivateKey(password: string) {
     const privateKey = await this.encryptionTool.decrypt(
@@ -105,7 +165,31 @@ export class Wallet {
     return privateKey;
   }
   retrievepubKeyAndAddress() {
-    return { publicKey: this.publicKey, address: this.address };
+    return {
+      publicKey: this.formatPublicKeyForUserInteraction(),
+      address: this.address,
+    };
+  }
+  addAminoPrefixToPubKey(
+    publicKey: HexEncoded,
+    aminoPrefixHex: HexEncoded = 'EB5AE98721' // secp256k1 key type prefix
+  ): Buffer {
+    const pubKeyBuffer = Buffer.from(publicKey);
+    const aminoPrefixBuffer = Buffer.from(aminoPrefixHex, 'hex');
+    const combinedBuffers = Buffer.concat([aminoPrefixBuffer, pubKeyBuffer]);
+    return combinedBuffers;
+  }
+  formatPublicKeyForUserInteraction(
+    publicKey: HexEncoded = this.publicKey
+  ): string {
+    const aminoPrefixedPubKey: Buffer = this.addAminoPrefixToPubKey(publicKey);
+    const words = bech32.toWords(aminoPrefixedPubKey);
+    const encodedPublicKey = bech32.encode('ideppub', words, 200);
+    return encodedPublicKey;
+  }
+  bech32EncodePublicKey(publicKey: HexEncoded = this.publicKey): Buffer {
+    const aminoPrefixedPubKey: Buffer = this.addAminoPrefixToPubKey(publicKey);
+    return Buffer.from(bech32.toWords(aminoPrefixedPubKey));
   }
 }
 
@@ -115,3 +199,4 @@ const walletFactory = () => {
 };
 
 export const wallet = walletFactory();
+export interface WalletInterface extends Wallet {}
