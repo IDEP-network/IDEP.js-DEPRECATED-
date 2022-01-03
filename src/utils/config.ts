0a703899130322def9c9683f30738032c1fff7d1
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-01-03 02:53:09 +0100

    Update config.ts
    this will have to go soon

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,4 @@
 export const config = {
- chainId: 'Test-Denali',
- lcdUrl: 'http://142.93.65.220:1317',
- Bech32Prefix: 'idep',
- hdPath: "m/44'/118'/0'/0/0",
+  Bech32Prefix: 'idep',
+  hdPath: "m/44'/118'/0'/0/0",
 } as const;
