9e1831b8d58ec0209c4dbad6a6579798a5b92969
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-01-12 18:40:53 +0100

    Update is-node.ts

--- src/utils/is-node.ts
+++ src/utils/is-node.ts
@@ -1,9 +1,7 @@
-const isNode = () => {
+export const isNode = () => {
   const isNode =
     typeof process !== 'undefined' &&
     process.versions != null &&
     process.versions.node != null;
   return isNode;
 };
-
-export default isNode();
