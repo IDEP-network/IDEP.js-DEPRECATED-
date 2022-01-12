13caf5ccef23a1cdd07463548b859e21c404a75b
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-01-12 20:56:54 +0100

    Update hashing.tools.ts

--- src/cryptography/hashing.tools.ts
+++ src/cryptography/hashing.tools.ts
@@ -1,18 +1,15 @@
 import RIPEMD160 from 'ripemd160';
 import shajs from 'sha.js';
 
-const Buffer = require('buffer/');
-
 export const sha256 = (bytes: Uint8Array | Buffer): Buffer => {
-  const buffer = bytes instanceof Buffer ? bytes : Buffer.from(bytes);
-  const hash: Buffer = shajs('sha256')
-    .update(buffer)
+  const hash = shajs('sha256')
+    .update(bytes)
     .digest();
   return hash;
 };
 
 export const ripemd160 = (bytes: Uint8Array | Buffer): Buffer => {
-  const buffer = bytes instanceof Buffer ? bytes : Buffer.from(bytes);
+  const buffer = Buffer.from(bytes);
   const hash = new RIPEMD160().update(buffer).digest();
   return hash;
 };
