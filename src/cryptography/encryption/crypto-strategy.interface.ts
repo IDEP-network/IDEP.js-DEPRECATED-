2ef6782d0f726044a5f898b830dea1f66d70dee6
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-01-12 18:28:37 +0100

    Update crypto-strategy.interface.ts
    formatting

--- src/cryptography/encryption/crypto-strategy.interface.ts
+++ src/cryptography/encryption/crypto-strategy.interface.ts
@@ -1,9 +1,9 @@
 export interface CryptoStrategy {
-	getRandomBytes(length: number): Promise<Buffer | Uint8Array>;
-	kdfMethod(
-			password: string,
-			salt: string | any
-	): Promise<Buffer | ArrayBuffer>;
-	encrypt(message: string, password: string): Promise<any>;
-	decrypt(encryptedphrase: any, password: string): Promise<string>;
+  getRandomBytes(length: number): Buffer | Uint8Array;
+  kdfMethod(
+    password: string,
+    salt: string | any
+  ): Promise<Buffer | ArrayBuffer>;
+  encrypt(message: string, password: string): Promise<any>;
+  decrypt(encryptedphrase: any, password: string): Promise<string>;
 }
