57005ad2bb09dd5a1bfdd2f070f9b0b7f70e14bb
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-01-12 19:48:00 +0100

    Update wallet.tools.ts
    formatting, couple small changes

--- src/wallet/wallet.tools.ts
+++ src/wallet/wallet.tools.ts
@@ -1,35 +1,33 @@
 import * as walletCreationTool from './wallet.helper';
 
-
-
 export class WalletTools {
-	static async generateWallet() {
-			const mnemonic = await walletCreationTool.generateMnemonic();
-			const {
-					privateKey,
-					publicKey,
-					address,
-			} = await WalletTools.generateFromSeed(mnemonic);
-			return { mnemonic, privateKey, publicKey, address };
-	}
-	static async recoverFromMnemonics(mnemonic: string) {
-			return WalletTools.generateFromSeed(mnemonic);
-	}
-	static async generateFromSeed(mnemonic: string) {
-			const privateKey = await walletCreationTool.generatePrivateKeyFromMnemonic(
-					mnemonic
-			);
-			const publicKey = walletCreationTool.derivePublicKeyFromPrivateKey(
-					privateKey
-			);
-			const address = await walletCreationTool.getAddressFromPublicKey(publicKey);
-			return { privateKey, publicKey, address };
-	}
-	static async recoverFromPrivateKey(privateKey: string) {
-			const publicKey = walletCreationTool.derivePublicKeyFromPrivateKey(
-					Buffer.from(privateKey, 'hex')
-			);
-			const address = await walletCreationTool.getAddressFromPublicKey(publicKey);
-			return { privateKey, publicKey, address };
-	}
+  static async generateWallet() {
+    const mnemonic = await walletCreationTool.generateMnemonic();
+    const {
+      privateKey,
+      publicKey,
+      address,
+    } = await WalletTools.generateFromSeed(mnemonic);
+    return { mnemonic, privateKey, publicKey, address };
+  }
+  static async recoverFromMnemonics(mnemonic: string) {
+    return WalletTools.generateFromSeed(mnemonic);
+  }
+  static async generateFromSeed(mnemonic: string) {
+    const privateKey = await walletCreationTool.generatePrivateKeyFromMnemonic(
+      mnemonic
+    );
+    const publicKey = walletCreationTool.derivePublicKeyFromPrivateKey(
+      privateKey
+    );
+    const address = walletCreationTool.getAddressFromPublicKey(publicKey);
+    return { privateKey, publicKey, address };
+  }
+  static async recoverFromPrivateKey(privateKey: string) {
+    const publicKey = walletCreationTool.derivePublicKeyFromPrivateKey(
+      Buffer.from(privateKey, 'hex')
+    );
+    const address = walletCreationTool.getAddressFromPublicKey(publicKey);
+    return { privateKey, publicKey, address };
+  }
 }
