620502287a92a62b2329528ad71613c98ff8ac56
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-01-21 18:23:38 +0100

    Update tsdx.config.js

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,15 +1,17 @@
+const nodePolyfills = require('rollup-plugin-polyfill-node');
 const replace = require('rollup-plugin-replace');
 const envType = process.env.ENV_TYPE;
-
 module.exports = {
   rollup(config) {
     // removes the esm prefix -> that just makes importing the module more difficult
     config.output.file = config.output.file.replace('.esm', '');
 
-    if (envType)
+    if (envType) {
       config.plugins.push(
         replace({ 'process.envType': `'${envType}'` }),
+        nodePolyfills({ include: null })
       );
+    }
     return config;
   },
 };
