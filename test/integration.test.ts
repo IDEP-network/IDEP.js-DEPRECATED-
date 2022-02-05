7d7694f121ae278596847487b008ea32ae117841
radekxrandom <12987981+radekxrandom@users.noreply.github.com>
2022-02-05 13:35:01 +0100

    Update integration.test.ts

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,46 +1,57 @@
 import {ClientInterface, createNewClient} from '../src/client/client';
 
-/**
- * @jest-environment jsdom
- */
 describe('Client', () => {
   let client: ClientInterface;
   beforeAll(() => {
-    client = createNewClient();
+    client = createNewClient({ nodeUrl: 'http://143.110.246.141:26657' });
   });
   describe('Wallet tests', () => {
     describe('Wallet can be createed, persisted, and retrieved', () => {
       it('Create and persist wallet', async () => {
-        await client.wallet.createNew('encryptionPwd', 'name');
-        console.log(client.wallet);
-        await client.wallet.persistWallet();
+        const wallet = await client.wallet.createNew('encryptionPwd', 'name');
+        console.log(wallet);
+        expect(client.wallet.name).toEqual('name');
+        expect(wallet.mnemonic.split(' ').length).toEqual(18);
         // wallete name can bee provided wheen persisting one
         //await client.wallet.persistWallet('walletName');
       });
       it('Retrieve that wallet', async () => {
-        await client.wallet.retrieveSavedWallet('name');
-        console.log(client.wallet);
-      });
-      it('Decrypt the private key', async () => {
-        const key = await client.wallet.getPrivateKey('encryptionPwd');
-        console.log(key);
+        const wallet = await client.wallet.retrieveSavedWallet('name');
+        console.log(wallet);
+        expect(client.wallet.name).toEqual('name');
       });
       it('Check bip32, bip39, wallet generation', async () => {
         const w = await client.wallet.restoreWithSeed(
           'disorder suffer swing screen copy calm fork legal strategy until home ancient moment bench wet present sure vote match pluck peanut odor shoe monster',
           'pindi'
         );
-        console.log(client.wallet);
         console.log(w);
         expect(w.publicKey).toBe('ideppub1addwnpepqwdf3jg3u3j4y5nqj3qzx0y4fc3cccglql59sl7guc280fehntftct03e9c');
         expect(w.address).toBe('idep1w574qn3yqgt0vsae3mut2upm9wjs5rzvw2y2h4');
+      });
+    });
+    describe('Restoree with seed, check private keey decryption', () => {
+      it('Restoree', async () => {
+        const w = await client.wallet.restoreWithSeed(
+          'power thing inmate obscure rubber frequent grit hair below museum notable reopen spoon prize family caught axis host',
+          'password'
+        );
+        console.log(w);
+        expect(w.publicKey).toBe('ideppub1addwnpepqftm06dn5ndkvccz645a0t7y2rz34qgqjea4xsn37wzdl40u4t3n2j6l8sj');
+        expect(w.address).toBe('idep1kt2kuzzn7nm3zr8qnmnh7qez55tc67hzhkjn9z');
+      });
+
+      it('Decrypt the private key', async () => {
+        const key = await client.wallet.getPrivateKey('password');
+        console.log(key);
+        expect(key).toEqual('4c63f44a4d5f1d51d40b029438e231abe409d1b8bbd6ba988420a4054718e48c');
       });
     });
   });
   describe('Querying works', () => {
     it('query bank balance', async () => {
       const res = await client.bank.checkBalance(
-        'idep126dfeu0d6awmdjy29e4f04eg0g3kvcpz9dazru',
+        'idep1kt2kuzzn7nm3zr8qnmnh7qez55tc67hzhkjn9z',
         'idep'
       );
       console.log(res);
@@ -65,7 +76,6 @@ describe('Client', () => {
         'power thing inmate obscure rubber frequent grit hair below museum notable reopen spoon prize family caught axis host',
         'password'
       );
-      console.log(client.wallet);
     });
     it('Send MsgSend tx', async () => {
       const txResult = await client.bank.msgSend(
